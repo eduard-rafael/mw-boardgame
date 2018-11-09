c99e4e1c7d6d0a93aa9d4ea940882cac24e90a42
eduardrafaelscmspain <eduard.rafael@scmspain.com>
2018-11-09 13:32:30 +0100

    First Commit

---
+++ src/test/kotlin/com/eraprojects/mwboardgame/MwboardgameApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.eraprojects.mwboardgame
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class MwboardgameApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
